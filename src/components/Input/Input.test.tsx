5986363c0ad5df4514661c4422c5efe4b08faadd
Adsyarif <adrisansyarif@gmail.com>
2024-01-27 13:49:59 +0700

    add onChange testing

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,27 +1,66 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Input from ".";
+
 describe("Input component unit testing", () => {
   const mocking = {
     onChange: jest.fn(),
   };
-  test("Expect render correctly", () => {
-    const document = render(
+
+  test("renders correctly with default values", () => {
+    const { asFragment } = render(
       <Input
         placeHolder="testing-placeholder"
         handleOnChange={mocking.onChange}
+        value={undefined}
       />
     );
-    expect(document).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  test("Expect the placeholder to have right content", () => {
+  test("renders with the correct placeholder content", () => {
+    render(
+      <Input
+        placeHolder="testing-placeholder"
+        handleOnChange={mocking.onChange}
+        value={undefined}
+      />
+    );
+    const element = screen.getByPlaceholderText("testing-placeholder");
+    expect(element).toMatchSnapshot();
+  });
+
+  test("renders input with the initial value", () => {
+    render(
+      <Input
+        placeHolder="testing-placeholder"
+        handleOnChange={mocking.onChange}
+        value="test value"
+      />
+    );
+
+    const input = screen.getByPlaceholderText("testing-placeholder");
+
+    expect(input).toBeDefined();
+    expect(screen.getByDisplayValue("test value")).toMatchSnapshot();
+  });
+
+  test("calls onChange when input value changes", async () => {
     render(
       <Input
         placeHolder="testing-placeholder"
         handleOnChange={mocking.onChange}
       />
     );
-    const element = screen.getByPlaceholderText("testing-placeholder");
-    expect(element).toMatchSnapshot();
+
+    const input = screen.getByPlaceholderText("testing-placeholder");
+
+    // Change the input value
+    fireEvent.change(input, { target: { value: "new value" } });
+
+    // Check if the onChange function is called once with the new value
+    await waitFor(() => {
+      expect(mocking.onChange).toHaveBeenCalled();
+      //   expect(mocking.onChange).toHaveBeenCalledWith("new value");
+    });
   });
 });
